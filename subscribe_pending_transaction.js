e10213a2055e013250fa521783c384e34f0e8f90
Aindriu Mac Giolla Eoin <aindriu80@gmail.com>
2018-09-29 20:42:03 +0100

    Ether over a value of 1

--- subscribe_pending_transaction.js
+++ subscribe_pending_transaction.js
@@ -2,6 +2,23 @@ const Web3 = require("web3");
 const ws = "wss://mainnet.infura.io/ws";
 const web3 = new Web3(ws);
 
+// web3.eth.subscribe("pendingTransactions", (error, txhash) => {
+//   console.log(txhash);
+// });
+
 web3.eth.subscribe("pendingTransactions", (error, txhash) => {
-  console.log(txhash);
+  if (!error) {
+    web3.eth.getTransaction(txhash, (error, tx) => {
+      if (tx != null) {
+        // console.log(tx);
+        // filter transaction >= 1eth
+        if (tx.value >= 1000000000000000000) {
+          console.log(txhash);
+          console.log(web3.utils.fromWei(tx.value, "ether"), "ether");
+        }
+      }
+    });
+  } else {
+    console.log(error);
+  }
 });
